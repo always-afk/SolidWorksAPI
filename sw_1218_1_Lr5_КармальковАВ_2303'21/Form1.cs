bdafbaea0c3bbaf699296628d1b68d9d28264131
Артём Кармальков <artemkarmalkov@gmail.com>
2021-03-23 14:43:43 +0300

    check Solid

--- sw_1218_1_Lr5_КармальковАВ_2303'21/Form1.cs
+++ sw_1218_1_Lr5_КармальковАВ_2303'21/Form1.cs
@@ -7,14 +7,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using System.Runtime.InteropServices;
 
 namespace sw_1218_1_Lr5_КармальковАВ_2303_21
 {
     public partial class Form1 : Form
     {
+        SldWorks swApp;
+        ModelDoc2 swModel;
+        SketchManager swSketchManager;
+        SelectionMgr swSelMgr;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        void buttonDrawAll_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private Boolean GetSolidworks()
+        {
+            try
+            {
+                // Присваиваем переменной ссылку на запущенный solidworks (по названию)
+                swApp = (SldWorks)Marshal.GetActiveObject("SldWorks.Application");
+            }
+            catch
+            {
+                // Отображает окно сообщения с заданным текстом
+                MessageBox.Show("Не удалось найти запущенный Solidworks!");
+                return false;
+            }
+
+            if (swApp.ActiveDoc == null)
+            {
+                // Отображает окно сообщения с заданным текстом
+                MessageBox.Show("Надо открыть деталь перед использованием");
+                return false;
+            }
+
+
+            // Присваиваем переменной ссылку на открытый активный проект в  SolidWorks
+            swModel = (ModelDoc2)swApp.ActiveDoc;
+
+            // Получает ISketchManager объект, который позволяет получить доступ к процедурам эскиза
+            swSketchManager = (SketchManager)swModel.SketchManager;
+
+            // Получает ISelectionMgr объект для данного документа, что делает выбранный объект доступным
+            swSelMgr = (SelectionMgr)swModel.SelectionManager;
+
+
+            // Проверка на открытие именно детали в SolidWorks
+            if (swModel.GetType() != (int)swDocumentTypes_e.swDocDRAWING)
+            {
+                string text = "Это работает только на уровне чертежа";
+                // Отображает окно сообщения с заданным текстом
+                MessageBox.Show(text, "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+
+            return true;
         }
     }
 }
