5376193f9999f67edaeba4247511cb9d9908c8a1
Артём Кармальков <artemkarmalkov@gmail.com>
2021-03-23 09:38:49 +0300

    Добавьте файлы проекта.

---
+++ sw_1218_1_Lr5_КармальковАВ_2303'21/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sw_1218_1_Lr5_КармальковАВ_2303_21
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
